283f807678ab4897e42f90c2e4d8edbd8400a6e8
Alumno <Alumno@LABSI18>
2015-10-06 12:02:25 -0500

    grupo8

---
+++ NewPythonProject/src/newpythonproject.py
@@ -0,0 +1,31 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+
+a=int(input("Ingrese primer numero "))
+b=int(input("Ingrese segundo numero "))
+c=int(input("Ingrese tercer numero "))
+d=int(input("Ingrese cuarto numero "))
+e=int(input("Ingrese quinto numero "))
+mayor=0
+if((a>mayor)):
+    mayor=a
+if((b>mayor)):
+    mayor=b
+if((c>mayor)):
+    mayor=c
+if((d>mayor)) :
+    mayor=d
+if((e>mayor)):
+    mayor=e
+print()
+print("El numero mayor es " +str(mayor))
+    
+
+
+    
+    
+    
+  
+
